2da912d4c04ffb426e43f69443acf277b5882c88
rutwik <rutwikdesh28@gmail.com>
2021-05-26 19:31:55 +0530

    settings.py modified for heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-52ij#1m9*^te%eltuqx3c0d*y&#nuhq1-9db_q6x!8*ie=o@9-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'https://the-merch-perch.herokuapp.com/']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- mysite/urls.py
+++ mysite/urls.py
@@ -4,6 +4,8 @@ from django.urls.conf import include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.views.static import serve
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
@@ -11,5 +13,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
     path('shop/', include('shop.urls')),
-    path('blog/', include('blog.urls'))
+    path('blog/', include('blog.urls')),
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
