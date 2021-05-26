625edf0832874e1f12a0bd41efe87e6709f24175
rutwik <rutwikdesh28@gmail.com>
2021-05-26 23:16:51 +0530

    gg

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,8 +125,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Managing media
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
