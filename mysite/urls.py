bb25741e41987f0c3cb67ac0c938938a9a41eafa
rutwik deshmukh <rutwikdesh28@gmail.com>
2022-02-05 20:12:35 +0530

    added new tv

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from django.views.static import serve
-from django.conf.urls import url
+from django.urls import include, re_path
 from . import views
 
 urlpatterns = [
@@ -15,6 +15,6 @@ urlpatterns = [
     path('shop/', include('shop.urls')),
     path('blog/', include('blog.urls')),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
